0985d38429f9c76e93beeebdce904e94796ffce3
Filadelico <nestor_05ribero@hotmail.com>
2023-07-31 20:20:33 -0500

    update

---
+++ Julio-28/relaciones/src/main/java/com/ayudantia/relaciones/models/Mensajes.java
@@ -0,0 +1,56 @@
+package com.ayudantia.relaciones.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "mensajes")
+public class Mensajes {
+
+  @Id //PK
+  @GeneratedValue(strategy = GenerationType.IDENTITY) //AI autoincrement
+  private Long id;
+
+  @Size(min = 1, max = 128)
+  private String contenido;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "receptor_id")
+  private User receptor;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "remitende_id")
+  private User remitente;
+
+  @Column(updatable = false)
+  @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+  private Date created_at;
+
+  @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+  private Date updated_at;
+
+  /* DEFAULT NOW() */
+  @PrePersist
+  protected void onCreate() {
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }
+
+  /* NOW() ON UPDATE NOW() */
+  @PreUpdate
+  protected void onUpdate() {
+    this.updated_at = new Date();
+  }
+}
